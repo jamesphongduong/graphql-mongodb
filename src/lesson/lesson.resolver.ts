6c34797f04c30e503132598f60ca5e486a272ee6
James <james.duong93@gmail.com>
2020-07-12 18:59:00 +1000

    getLesson query

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -6,13 +6,8 @@ import { LessonService } from './lesson.service';
 export class LessonResolver {
   constructor(private lessonService: LessonService) {}
   @Query(returns => LessonType)
-  lesson() {
-    return {
-      id: 'asdjo12j31a',
-      name: 'Physics Class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
   }
 
   @Mutation(returns => LessonType)
